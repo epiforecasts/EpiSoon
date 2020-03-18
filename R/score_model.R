f9c2f05f819c618ae2ee4ee98e7753e726079b83
seabbs <s.e.abbott12@gmail.com>
2020-03-18 17:51:37 +0000

    added model scoring

---
+++ R/score_model.R
@@ -0,0 +1,59 @@
+
+#' Score a Model Fit
+#'
+#' @param observations A dataframe of observations against which to score. Should contain a `date` and `rt` column.
+#'
+#' @return A dataframe containing the following scores per forecast timepoint: dss, crps,
+#' logs, bias, and sharpness as well as the forecast date and time horizon.
+#' @export
+#'
+#' @importFrom dplyr filter select
+#' @importFrom tidyr spread
+#' @importFrom tibble tibble
+#' @importFrom scoringRules dss_sample crps_sample logs_sample
+#' @importFrom scoringutils bias sharpness
+#' @inheritParams summarise_fit
+#' @examples
+#'
+#' ## Observed data
+#' observations <- data.frame(rt = 1:20,
+#'                            date = as.Date("2020-01-01")
+#'                                   + lubridate::days(1:20))
+#'
+#' ## Fit a model
+#' samples <- fit_model(observations[1:10, ],
+#'                      model = bsts::AddAutoAr,
+#'                      horizon = 7, samples = 10)
+#'
+#' ## Score the model fit
+#' score_model(samples, observations[-c(1:10), ])
+score_model <- function(fit_samples, observations) {
+
+  observations <- observations %>%
+    dplyr::filter(
+      date >= min(fit_samples$date),
+      date <= max(fit_samples$date)
+    )
+
+  obs <- observations$rt
+
+  samples_matrix <- fit_samples %>%
+    tidyr::spread(key = "sample", value = "rt") %>%
+    dplyr::select(-horizon, -date) %>%
+    as.matrix
+
+  scores <- tibble::tibble(
+    date = observations$date,
+    horizon = 1:length(observations$date),
+    dss = scoringRules::dss_sample(y = obs, dat = samples_matrix),
+    crps = scoringRules::crps_sample(y = obs, dat = samples_matrix),
+    logs = scoringRules::logs_sample(y = obs, dat = samples_matrix),
+    bias = suppressWarnings(
+      scoringutils::bias(obs, samples_matrix)
+      ),
+    sharpness = scoringutils::sharpness(samples_matrix)
+  )
+
+
+  return(scores)
+}
---
+++ R/summarise_fit.R
@@ -0,0 +1,35 @@
+#' Summarise Predicted Rts
+#'
+#' @param fit_samples A dataframe as produced by `EpiSoon::fit_model`.
+#'
+#' @return A summarised dataframe.
+#' @export
+#'
+#' @importFrom dplyr group_by summarise
+#' @examples
+#'
+#' rts <- data.frame(rt = 1:10,
+#'                  date = as.Date("2020-01-01") + lubridate::days(1:10))
+#'
+#'
+#' samples <- fit_model(rts, model = bsts::AddAutoAr, horizon = 7, samples = 10)
+#'
+#'
+#' summarise_fit(samples)
+summarise_fit <- function(fit_samples) {
+
+  summarised_fit <- fit_samples %>%
+    dplyr::group_by(date, horizon) %>%
+    dplyr::summarise(
+      bottom = quantile(rt, 0.025, na.rm = TRUE),
+      lower =  quantile(rt, 0.25, na.rm = TRUE),
+      median =  median(rt, na.rm = TRUE),
+      mean = mean(rt, na.rm = TRUE),
+      upper = quantile(rt, 0.75, na.rm = TRUE),
+      top = quantile(rt, 0.975, na.rm = TRUE),
+      sd = sd(rt, na.rm = TRUE)
+    ) %>%
+    ungroup()
+
+  return(summarised_fit)
+}
