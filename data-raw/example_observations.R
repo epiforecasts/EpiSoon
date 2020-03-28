8c66d58d88557211c184465b5749a868f62cf0da
seabbs <s.e.abbott12@gmail.com>
2020-03-28 11:45:57 +0000

    further improvements to example data - needs to have the same start date

--- data-raw/example_observations.R
+++ data-raw/example_observations.R
@@ -4,26 +4,28 @@
 library(dplyr)
 library(usethis)
 
-# Add cases data -------------------------------------------------------
-
-example_obs_cases <- readRDS("data-raw/obs_cases.rds") %>%
-  dplyr::filter(timeseries %in% "austria") %>%
-  dplyr::select(-timeseries) %>%
-  dplyr::slice(30:63)
-
-
-usethis::use_data(example_obs_cases, overwrite = TRUE)
-
-
-
 # Add rts  ----------------------------------------------------------------
 
 
 example_obs_rts <- readRDS("data-raw/obs_rts.rds") %>%
   dplyr::filter(timeseries %in% "austria") %>%
   dplyr::select(-timeseries) %>%
-  dplyr::slice(30:63)
+  dplyr::slice(-c(1:29))
 
 
 usethis::use_data(example_obs_rts, overwrite = TRUE)
+
+
+
+# Add cases data -------------------------------------------------------
+
+example_obs_cases <- readRDS("data-raw/obs_cases.rds") %>%
+  dplyr::filter(timeseries %in% "austria") %>%
+  dplyr::select(-timeseries) %>%
+  dplyr::filter(date >= min(example_obs_rts$date))
+
+
+usethis::use_data(example_obs_cases, overwrite = TRUE)
+
+
 
