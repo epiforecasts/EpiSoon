1d415b73150681a4f55c805af64c6a43fc536909
seabbs <s.e.abbott12@gmail.com>
2020-03-28 11:39:30 +0000

    summarise examples updated and working

--- R/summarise_case_forecast.R
+++ R/summarise_case_forecast.R
@@ -9,28 +9,18 @@
 #' @importFrom HDInterval hdi
 #' @examples
 #'
-#' ## Observed cases
-#' cases <- data.frame(date = seq(as.Date("2020-01-01"),
-#'                                as.Date("2020-01-10"),
-#'                                by = "days"),
-#'                     cases = 1:10)
-#'
-#' ## Forecast Rts
-#' rts <- data.frame(date = seq(as.Date("2020-01-01"),
-#'                                as.Date("2020-01-10"),
-#'                                by = "days"),
-#'                    rt = rnorm(10, 1.2, 0.01))
-#'
-#'
-#' forecast <- forecast_rt(rts,
+#' ## Example forecast
+#' forecast <- forecast_rt(EpiSoon::example_obs_rts,
 #'                         model = function(ss, y) {
 #'                         bsts::AddAutoAr(ss, y = y, lags = 7)
 #'                         },
 #'                         horizon = 7, samples = 10)
 #'
-#'
-#' case_forecast <- forecast_cases(cases, forecast, EpiSoon::example_serial_interval)
-#'
+#' ## Forecast cases
+#' case_forecast <- forecast_cases(EpiSoon::example_obs_cases,
+#'                                 forecast,
+#'                                 EpiSoon::example_serial_interval)
+#' ## Summarise case forecast
 #' summarise_case_forecast(case_forecast)
 summarise_case_forecast <- function(pred_cases) {
 
