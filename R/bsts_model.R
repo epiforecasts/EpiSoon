528c33c5f631b32ab6814c3a331eceffd936f214
seabbs <s.e.abbott12@gmail.com>
2020-04-03 11:46:40 +0000

    base level implementation of bsts as a passed object

---
+++ R/bsts_model.R
@@ -0,0 +1,44 @@
+#' BSTS model wrapper
+#'
+#' @param y Numeric vector of time points to forecast
+#' @param samples Numeric, number of samples to take.
+#' @param model A `bsts` model object wrapped in a function with an `ss` and `y` argument.
+#' @param horizon Numeric, the time horizon over which to predict.
+#' @return A dataframe of predictions (with columns representing the time horizon and rows representing samples).
+#' @export
+#' @importFrom bsts bsts predict.bsts
+#' @examples
+#'
+#'
+#'forecast_rt(EpiSoon::example_obs_rts[1:10, ],
+#'            model = function(...){bsts_model(model = function(ss, y){bsts::AddAutoAr(ss, y = y, lags = 10)}, ...)},
+#'            horizon = 7, samples = 10)
+bsts_model <- function(y = NULL, samples = NULL,
+                       horizon = NULL, model = NULL) {
+
+
+  model <- model(list(), y)
+
+  ## Fit the model
+  fitted_model <- bsts::bsts(y,
+               state.specification = model,
+               niter = ifelse(samples < 100, 100 + samples, samples * 2),
+               ping = 0)
+
+
+  ## Predict using the model
+  prediction <- bsts::predict.bsts(fitted_model, horizon = horizon,
+                                   burn = ifelse(samples < 100, 100, samples),
+                                   quantiles = c(.025, .975))
+
+  ## Extract samples and tidy format
+  samples <- as.data.frame(prediction$distribution)
+
+  return(samples)
+
+
+
+
+
+
+}
--- R/forecast_rt.R
+++ R/forecast_rt.R
@@ -2,17 +2,12 @@
 #'
 #' @param rts A dataframe of containing two variables `rt` and  `date` with
 #' `rt` being numeric and `date` being a date.
-#' @param model An uninitialised `bsts` model
-#' @param horizon Numeric, the time horizon over which to predict
-#' @param samples Numeric, the number of samples to from the posterior of the model
-#' fit. Note that twice this number of MCMC steps will be taken with half used as burn in.
+#' @param model A model object in the format of `bsts_model` etc.
 #' @param bound_rt Logical, defaults to `TRUE`. Should Rt values be bounded to be greater than or
 #' equal to 0.
-#' @param timeout Numeric, the number of seconds to allow before terminating model fitting. Defaults to
-#' 30 seconds.
+#' @param timeout Numeric, timeout of model fitting in seconds. Defaults to 30 seconds.
 #' @return A dataframe of samples containing the following variables:
 #'  `sample`, `date`, `rt`, and `horizon`.
-#' @importFrom bsts bsts predict.bsts
 #' @importFrom lubridate days
 #' @importFrom dplyr mutate n group_by ungroup
 #' @importFrom tidyr gather
@@ -21,7 +16,7 @@
 #'
 #' @examples
 #' forecast_rt(EpiSoon::example_obs_rts[1:10, ],
-#'             model = function(ss, y){bsts::AddAutoAr(ss, y = y, lags = 10)},
+#'             model = function(...){bsts_model(model = function(ss, y){bsts::AddAutoAr(ss, y = y, lags = 10)}, ...)},
 #'             horizon = 7, samples = 10)
 forecast_rt <- function(rts, model = model,
                       horizon = 7, samples = 1000,
@@ -30,25 +25,11 @@ forecast_rt <- function(rts, model = model,
   ## Set up for model fitting
   y <- rts$rt
 
-  model <- model(list(), y)
-
-  ## Fit the model
-  fitted_model <- R.utils::withTimeout(
-    bsts::bsts(y,
-               state.specification = model,
-               niter = ifelse(samples < 100, 100 + samples, samples * 2),
-               ping = 0),
+  ## Forecast and return samples
+  samples <- R.utils::withTimeout(
+    model(y = y, samples = samples, horizon = horizon),
     timeout = timeout, onTimeout = "error"
   )
-
-
-  ## Predict using the model
-  prediction <- bsts::predict.bsts(fitted_model, horizon = horizon,
-                                   burn = ifelse(samples < 100, 100, samples),
-                                   quantiles = c(.025, .975))
-
-  ## Extract samples and tidy format
-  samples <- as.data.frame(prediction$distribution)
 
   colnames(samples) <- max(rts$date) + lubridate::days(1:horizon)
 
