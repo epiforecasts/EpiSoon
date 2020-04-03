a871a42fdfce62ea55baae2f9c8c2cc464918c7c
seabbs <s.e.abbott12@gmail.com>
2020-04-03 11:56:54 +0000

    update compare_models to work with new wrapper

--- R/compare_models.R
+++ R/compare_models.R
@@ -1,8 +1,9 @@
 #' Compare forecasting models
 #'
-#' @param models A list of `bsts` models. An example configuration is given in the
-#' examples. Each `bsts` model needs to be wrapped in a function that takes a `ss` and `y`
-#' argument (i.e. `function(ss, y){bsts::AddSemilocalLinearTrend(ss, y = y)}`).
+#' @param models A list of models. A configuration is given in the
+#' examples. Each model needs to be wrapped in a function that takes a `...` argument and returns a dataframe
+#' of samples with each column representing a time horizon.
+#' Example: `function(...) {EpiSoon::bsts_model(model = function(ss, y){bsts::AddAr(ss, y = y, lags = 3)}, ...)}`.
 #'
 #' @inheritParams evaluate_model
 #' @return A list of dataframes as produced by `evaluate model` but with an additional model column.
@@ -14,16 +15,18 @@
 #'
 #' ## List of forecasting bsts models wrapped in functions.
 #' models <- list("AR 3" =
-#'                     function(ss, y){bsts::AddAr(ss, y = y, lags = 3)},
+#'                     function(...) {EpiSoon::bsts_model(model =
+#'                     function(ss, y){bsts::AddAr(ss, y = y, lags = 3)}, ...)},
 #'                "Semi-local linear trend" =
-#'                     function(ss, y){bsts::AddSemilocalLinearTrend(ss, y = y)})
+#'                function(...) {EpiSoon::bsts_model(model =
+#'                     function(ss, y){bsts::AddSemilocalLinearTrend(ss, y = y)}, ...)})
 #'
 #'
 #'
 #' ## Compare models
 #' evaluations <- compare_models(EpiSoon::example_obs_rts,
 #'                               EpiSoon::example_obs_cases, models,
-#'                               horizon = 7, samples = 10,
+#'                               horizon = 7, samples = 100,
 #'                               serial_interval = example_serial_interval)
 #'
 #' ## Example evaluation plot for comparing forecasts
