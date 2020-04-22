92ceb06cf0715152e0bfe84da0a4239483e54fe3
andreamrau <andrea.rau@gmail.com>
2020-04-22 11:54:37 +0200

    Add forecast_rt tests

---
+++ tests/testthat/test_forecast_rt.R
@@ -0,0 +1,46 @@
+##------------------------------------------------------------
+context("Test of the function 'forecast_rt'.... testing outputs")
+
+mod_test <- function(...) {
+  EpiSoon::bsts_model(model = function(ss, y){bsts::AddAutoAr(ss, y = y, lags = 10)}, ...)
+}
+rts_test <- EpiSoon::example_obs_rts[1:10, ]
+horizon_test <- 7
+samples_test <- 10
+run_test <- forecast_rt(rts = rts_test,
+            model = mod_test,
+            horizon = horizon_test,
+            samples = samples_test,
+            bound_rt = TRUE,
+            timeout = 30)
+test_that("Output has proper length, names, class", {
+  expect_type(run_test, "list")
+  expect_named(run_test)
+  expect_equal(nrow(run_test), horizon_test*samples_test)
+  expect_length(run_test, 4)
+  expect_named(run_test, c("sample", "date", "rt", "horizon"))
+})
+test_that("bound_rt = TRUE has appropriate bounding behavior", {
+  expect_gte(min(run_test$rt), 0)
+})
+
+##------------------------------------------------------------
+context("Test of the function 'forecast_rt'.... testing inputs")
+misspecified_model <- function(...) {
+  lm(y~x)
+}
+
+test_that("Outputs returns error for inappropriately specified model", {
+  expect_error(forecast_rt(rts = rts_test,
+                           model = misspecified_model,
+                           horizon = horizon_test,
+                           samples = samples_test,
+                           bound_rt = TRUE,
+                           timeout = 30))
+})
+
+## Suggestions:
+##   add checks on inputs (timeout >= 0, samples >= 1 and integer, horizon >= 1 and integer,
+##   model input is either bsts_model or fable_model (or at least has arguments of y, samples, and horizon),
+##   rts is indeed a data frame with the appropriate variables (rt, being numeric, and date, being a date),
+##   bount_rt is logical
