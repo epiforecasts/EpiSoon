32a92a41b783b255b686d8d7b8231b7ca481cc78
Sylvain Mareschal <mareschal@ovsa.fr>
2020-04-26 13:38:49 +0200

    Unit tests group 2

--- tests/testthat/test_forecast_cases.R
+++ tests/testthat/test_forecast_cases.R
@@ -16,6 +16,8 @@ forecast <- forecast_rt(
 	samples = 10
 )
 
+
+
 test_that("forecast_cases() output is of expected format", {
 	# Provided example
 	out <- forecast_cases(
@@ -27,12 +29,14 @@ test_that("forecast_cases() output is of expected format", {
 	expect_s3_class(out, c("tbl_df", "tbl", "data.frame"))
 	expect_named(out, c("sample", "date", "cases", "horizon"))
 	expect_equal(nrow(forecast), nrow(out))
-
+	
+	# FIXME : recommended to preserve original order
 	forecast <- forecast[ order(forecast$sample, forecast$date) ,]
-	expect_identical(
-		forecast[, c("sample","date","horizon") ],
-		out[, c("sample","date","horizon") ]
-	)
+	expect_equal(forecast$sample, out$sample)
+	expect_equal(forecast$date, out$date)
+	expect_equal(forecast$horizon, out$horizon)
+	
+	# FIXME : recommended to preserve column classes (sample casted from integer to numeric)
 })
 
 test_that("forecast_cases() handles missing arguments as expected", {
@@ -41,31 +45,69 @@ test_that("forecast_cases() handles missing arguments as expected", {
 		forecast_cases(),
 		"is missing"
 	)
-
+	
+	# FIXME : recommended to print a more explicit message 
 	expect_error(
 		forecast_cases(
 			fit_samples = forecast,
 			serial_interval = EpiSoon::example_serial_interval
-		),
-		"cases"
+		)
 	)
-
+	
+	# FIXME : recommended to print a more explicit message 
 	expect_error(
 		forecast_cases(
 			cases = EpiSoon::example_obs_cases,
 			serial_interval = EpiSoon::example_serial_interval
-		),
-		"fit_samples"
+		)
 	)
-
+	
+	# FIXME : recommended to print a more explicit message 
 	expect_error(
 		forecast_cases(
 			cases = EpiSoon::example_obs_cases,
 			fit_samples = forecast
-		),
-		"serial_interval"
+		)
 	)
-
+	
+	expect_identical(
+		{
+			set.seed(42)
+			forecast_cases(
+				EpiSoon::example_obs_cases,
+				fit_samples = forecast,
+				serial_interval = EpiSoon::example_serial_interval
+			)
+		},{
+			set.seed(42)
+			forecast_cases(
+				EpiSoon::example_obs_cases,
+				fit_samples = forecast,
+				serial_interval = EpiSoon::example_serial_interval,
+				forecast_date = forecast$date[1]
+			)
+		}
+	)
+	
+	expect_identical(
+		{
+			set.seed(42)
+			forecast_cases(
+				EpiSoon::example_obs_cases,
+				fit_samples = forecast,
+				serial_interval = EpiSoon::example_serial_interval
+			)
+		},{
+			set.seed(42)
+			forecast_cases(
+				EpiSoon::example_obs_cases,
+				fit_samples = forecast,
+				serial_interval = EpiSoon::example_serial_interval,
+				horizon = max(forecast$horizon)
+			)
+		}
+	)
+	
 	expect_identical(
 		{
 			set.seed(42)
@@ -85,26 +127,9 @@ test_that("forecast_cases() handles missing arguments as expected", {
 		}
 	)
 
-	expect_identical(
-		{
-			set.seed(42)
-			forecast_cases(
-				EpiSoon::example_obs_cases,
-				fit_samples = forecast,
-				serial_interval = EpiSoon::example_serial_interval
-			)
-		},{
-			set.seed(42)
-			forecast_cases(
-				EpiSoon::example_obs_cases,
-				fit_samples = forecast,
-				serial_interval = EpiSoon::example_serial_interval,
-				horizon = max(forecast$horizon)
-			)
-		}
-	)
-
 })
+
+# FIXME : 'horizon' appears to have no effect <https://github.com/epiforecasts/EpiSoon/issues/42>
 
 test_that("forecast_cases() can handle custom sampling functions", {
 
@@ -122,42 +147,6 @@ test_that("forecast_cases() can handle custom sampling functions", {
 	expect(rpois)
 	expect(rnorm)
 	expect(function(n,mean) { sample(-5:5+mean, n, replace=TRUE) })
-
-})
-
-# To be clarified <https://github.com/epiforecasts/EpiSoon/issues/42>
-test_that("forecast_cases() handles properly 'horizon'", {
-
-	expect_false(
-		identical(
-			{
-				set.seed(42)
-				forecast_cases(
-					EpiSoon::example_obs_cases,
-					fit_samples = forecast,
-					serial_interval = EpiSoon::example_serial_interval,
-					horizon = 1
-				)
-			},{
-				set.seed(42)
-				forecast_cases(
-					EpiSoon::example_obs_cases,
-					fit_samples = forecast,
-					serial_interval = EpiSoon::example_serial_interval,
-					horizon = 5
-				)
-			}
-		)
-	)
-
-	expect_error(
-		forecast_cases(
-			EpiSoon::example_obs_cases,
-			fit_samples = forecast,
-			serial_interval = EpiSoon::example_serial_interval,
-			horizon = 10
-		)
-	)
 
 })
 
