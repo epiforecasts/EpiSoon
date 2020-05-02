143bafdf8909a94eba0827ab1a796eef898d2965
Michael DeWitt <me.dewitt.jr@gmail.com>
2020-05-02 19:02:00 -0400

    fixed missing link in documentation. seems to pass all checks locally

--- R/fable_model.R
+++ R/fable_model.R
@@ -1,8 +1,9 @@
 #' Fable model wrapper
 #'
 #'
-#' @description Provides an interface for models from the `fable` package. Note the [feasts::ARIMA] model
-#' requires the `feast` package. If `future` is being used `fable` will require `future.apply` in
+#' @description Provides an interface for models from the `fable` package.
+#' Note the `feasts::ARIMA` model requires the `feast` package. If `future`
+#' is being used `fable` will require `future.apply` in
 #' order to not silently fail.
 #'
 #' @param model A `fable` model object. For  models that use a formula interface time
