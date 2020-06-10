55b96f7f274c880695153e2f7645f2f06924162e
Nathalie Vialaneix <nathalie.vialaneix@inrae.fr>
2020-06-10 12:57:02 +0200

    added new tests for forecast_cases()

--- tests/testthat/test_forecast_cases.R
+++ tests/testthat/test_forecast_cases.R
@@ -29,13 +29,13 @@ test_that("forecast_cases() output is of expected format", {
 	expect_s3_class(out, c("tbl_df", "tbl", "data.frame"))
 	expect_named(out, c("sample", "date", "cases", "horizon"))
 	expect_equal(nrow(forecast), nrow(out))
-	
+
 	# FIXME : recommended to preserve original order
 	forecast <- forecast[ order(forecast$sample, forecast$date) ,]
 	expect_equal(forecast$sample, out$sample)
 	expect_equal(forecast$date, out$date)
 	expect_equal(forecast$horizon, out$horizon)
-	
+
 	# FIXME : recommended to preserve column classes (sample casted from integer to numeric)
 })
 
@@ -45,31 +45,31 @@ test_that("forecast_cases() handles missing arguments as expected", {
 		forecast_cases(),
 		"is missing"
 	)
-	
-	# FIXME : recommended to print a more explicit message 
+
+	# FIXME : recommended to print a more explicit message
 	expect_error(
 		forecast_cases(
 			fit_samples = forecast,
 			serial_interval = EpiSoon::example_serial_interval
 		)
 	)
-	
-	# FIXME : recommended to print a more explicit message 
+
+	# FIXME : recommended to print a more explicit message
 	expect_error(
 		forecast_cases(
 			cases = EpiSoon::example_obs_cases,
 			serial_interval = EpiSoon::example_serial_interval
 		)
 	)
-	
-	# FIXME : recommended to print a more explicit message 
+
+	# FIXME : recommended to print a more explicit message
 	expect_error(
 		forecast_cases(
 			cases = EpiSoon::example_obs_cases,
 			fit_samples = forecast
 		)
 	)
-	
+
 	expect_identical(
 		{
 			set.seed(42)
@@ -88,7 +88,7 @@ test_that("forecast_cases() handles missing arguments as expected", {
 			)
 		}
 	)
-	
+
 	expect_identical(
 		{
 			set.seed(42)
@@ -107,7 +107,7 @@ test_that("forecast_cases() handles missing arguments as expected", {
 			)
 		}
 	)
-	
+
 	expect_identical(
 		{
 			set.seed(42)
@@ -129,8 +129,6 @@ test_that("forecast_cases() handles missing arguments as expected", {
 
 })
 
-# FIXME : 'horizon' appears to have no effect <https://github.com/epiforecasts/EpiSoon/issues/42>
-
 test_that("forecast_cases() can handle custom sampling functions", {
 
 	expect <- function(FUN) {
@@ -150,3 +148,30 @@ test_that("forecast_cases() can handle custom sampling functions", {
 
 })
 
+test_that("forecast_cases() properly uses 'horizon'", {
+  set.seed(42)
+  predictions_h5 <- forecast_cases(
+    EpiSoon::example_obs_cases,
+    fit_samples = forecast,
+    serial_interval = EpiSoon::example_serial_interval,
+    horizon = 5
+  )
+  expect_equal(max(predictions_h5$horizon), 5)
+
+  set.seed(42)
+  predictions_default <- forecast_cases(
+    EpiSoon::example_obs_cases,
+    fit_samples = forecast,
+    serial_interval = EpiSoon::example_serial_interval
+  )
+
+  set.seed(42)
+  predictions_h10 <- forecast_cases(
+    EpiSoon::example_obs_cases,
+    fit_samples = forecast,
+    serial_interval = EpiSoon::example_serial_interval,
+    horizon = 10
+  )
+
+  expect_identical(predictions_h10, predictions_default)
+})
